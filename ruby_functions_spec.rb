2f2d2339f6f7147767754d8a6196cc583cabecd7
Euan Fullarton <euanfullarton7@gmail.com>
2017-03-08 09:33:32 +0000

    amended the expected value so test still works

--- ruby_functions_spec.rb
+++ ruby_functions_spec.rb
@@ -71,7 +71,7 @@ class Functions_Practice < MiniTest::Test
   # #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
     volume_result = volume_of_cube(3)
-    assert_equal(9, volume_result)
+    assert_equal(27, volume_result)
   end
 
   # #Given the radius of a sphere calculate the volume
